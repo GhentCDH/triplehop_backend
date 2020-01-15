bf51eb608647fc51ec3c71567b447a59594ebabb
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2020-01-15 10:47:22 +0100

    Performance improvements: one query per entity creation

--- scripts/utils.py
+++ scripts/utils.py
@@ -19,62 +19,43 @@ def intOrNone(input: str):
     return int(input)
 
 def addEntity(cur, propConf: Dict, userId: int, entityId: int, row: Tuple):
-    # Get id and update entityCount
-    cur.execute('''
-    SELECT entityCount.currentId
-    FROM app.entityCount
-    WHERE entityCount.id = %(entityId)s;
-    ''', {
+    query = []
+    params = {
         'entityId': entityId,
-    })
-    newId = cur.fetchone()[0] + 1
-
-    cur.execute('''
-    UPDATE app.entityCount
-    SET currentId = %(id)s
-    WHERE id = %(entityId)s;
-    ''', {
-        'entityId': entityId,
-        'id': newId,
-    })
-
-    # Verify the entity doen't exist
-    cur.execute('''
-    MATCH (ve:v%(entityId)s {id: %(id)s}) RETURN ve;
-    ''', {
-        'entityId': entityId,
-        'id': newId,
-    })
-    if cur.rowcount > 0:
-        # TODO: create a more usefull error message
-        raise Exception('Entity already exists.')
-    
-    # Create entity and initial revision
-    cur.execute('''
-    CREATE 
-        (:v%(entityId)s {id: %(id)s})
-        -[:erevision]->
-        (:vrevision {user_id: %(userId)s, revision_id: 1, timestamp: (SELECT EXTRACT(EPOCH FROM NOW()))})
-    ;
-    ''', {
-        'entityId': entityId,
-        'id': newId,
         'userId': userId,
-    })
+    }
+
+    # Get id and update entityCount
+    query.append('''
+    UPDATE app.entityCount
+    SET currentId = currentId + 1
+    WHERE id = %(entityId)s;
+    ''')
+
+    # Create entity and initial revision
+    query.append('''
+    CREATE 
+        (ve:v%(entityId)s {id: (SELECT currentId FROM app.entityCount WHERE id = %(entityId)s)})
+        -[:erevision]->
+        (vr:vrevision {user_id: %(userId)s, revision_id: 1, timestamp: (SELECT EXTRACT(EPOCH FROM NOW()))});
+    ''')
 
     # Add properties and corresponding relations
     for (key, indices) in propConf.items():
         if row[indices[1]] != '':
-            cur.execute('''
-            MATCH (ve:v%(entityId)s {id: %(id)s})-[:erevision]->(vr)
-                SET ve.%(propertyName)s = %(value)s
-                CREATE (ve)-[:eproperty]->(vp:v%(entityId)s_%(propertyId)s {value: %(value)s})
-                CREATE (vp)-[:erevision]->(vr)
-            ;
-            ''', {
-                'id': newId,
-                'entityId': entityId,
-                'propertyId': indices[0],
-                'propertyName': key,
-                'value': row[indices[1]],
-            })
+            query.append('''
+            SET ve.%(propertyName_{id})s = %(value_{id})s
+            CREATE (ve)-[:eproperty]->(vp_%(propertyId_{id})s:v%(entityId)s_%(propertyId_{id})s {{value: %(value_{id})s}})
+            CREATE (vp_%(propertyId_{id})s)-[:erevision]->(vr);
+            '''.format(id=indices[0]))
+            params[f'propertyId_{indices[0]}'] = indices[0]
+            params[f'propertyName_{indices[0]}'] = key
+            params[f'value_{indices[0]}'] = row[indices[1]]
+
+    # remove semicolons (present for code readibility only)
+    query = [q.replace(';', '') if i > 0 else q for i, q in enumerate(query)]
+
+    cur.execute(
+        '\n'.join(query) + ';',
+        params
+    )
