7696cf43f04f6032ce6677cd144a7298c22948f9
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2020-03-10 16:22:46 +0100

    Allow CORS request from the front-end

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1 import router as api_v1_router
 from app.db.core import db_connect, db_disconnect
 
 app = FastAPI()
+
+# TODO: load from config
+origins = [
+    'http://local.crdb:3000',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
