6be7e74f07c3291e26f16b1d451b2377b3f9567f
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2020-03-27 16:33:59 +0100

    Add sample authorization data

--- scripts/db_user_data.py
+++ scripts/db_user_data.py
@@ -11,4 +11,24 @@ with psycopg2.connect('dbname=crdb host=127.0.0.1 user=vagrant') as conn:
             'false'
         )
         ON CONFLICT DO NOTHING;
+
+        INSERT INTO app.group (system_name, display_name, description)
+        VALUES (
+            'global_admin',
+            'Global administrator',
+            'Users in this group have all permissions'
+        );
+
+        INSERT INTO app.permission (system_name, display_name, description)
+        VALUES (
+            'es_index',
+            'Index data in Elasticsearch',
+            'Index data in Elasticsearch'
+        );
+
+        INSERT INTO app.groups_permissions (group_id, permission_id)
+        VALUES (
+            (SELECT "group".id FROM app.group WHERE "group".system_name = 'global_admin'),
+            (SELECT permission.id FROM app.permission WHERE permission.system_name = 'es_index')
+        );
         ''')
