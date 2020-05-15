abcba6f82d139a57ccdc5dc4108ce00761655ca6
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2020-05-15 10:49:11 +0200

    Complete es reindex controller

--- app/router/es/v1.py
+++ app/router/es/v1.py
@@ -8,11 +8,12 @@ from app.db.job import JobRepository
 from app.es.job import reindex as reindex_job
 from app.models.auth import UserWithPermissions
 from app.models.es import ElasticSearchRequest
+from app.models.job import JobId
 
 router = APIRouter()
 
 
-@router.post('/reindex')
+@router.post('/reindex', response_model=JobId)
 async def reindex(
     es_request: ElasticSearchRequest,
     background_tasks: BackgroundTasks,
@@ -28,9 +29,6 @@ async def reindex(
         'es_index',
     )
     job_repository = await get_repository_from_request(request, JobRepository)
-    job_id = await job_repository.create_job(user, 'es_index', project_name, entity_type_name)
+    job_id = await job_repository.create(user, 'es_index', project_name, entity_type_name)
     background_tasks.add_task(reindex_job, job_id, project_name, entity_type_name, request)
-    # TODO: start background task: background_tasks.add_task(reindex_task, )
-    # TODO: return response
-    #
-    # return await data_repo.get_entity_ids_by_type_name(es_request.project_name, es_request.entity_type_name)
+    return JobId(id=job_id)
