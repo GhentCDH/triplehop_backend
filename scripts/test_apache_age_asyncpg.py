b85d24c26d7e2f8fc8eee17a069fec3ee13cff4b
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2021-03-15 16:33:23 +0100

    Add minimal asyncpg test

---
+++ scripts/test_apache_age_asyncpg.py
@@ -0,0 +1,41 @@
+import asyncio
+import asyncpg
+
+
+async def main():
+    # Don't use prepared statements (see https://github.com/apache/incubator-age/issues/28)
+    pool = await asyncpg.create_pool('postgresql://testuser:testpass@127.0.0.1:5433/testdb', statement_cache_size=0)
+    async with pool.acquire() as conn:
+        await conn.execute(
+            '''
+                SET search_path = ag_catalog, "$user", public;
+            '''
+        )
+
+        # await conn.execute(
+        #     '''
+        #         SELECT create_graph('testgraph');
+        #     '''
+        # )
+
+        transaction = conn.transaction()
+        try:
+            await transaction.start()
+            await conn.execute(
+                '''
+                    SELECT * FROM cypher('testgraph', $$MATCH (v) return v$$) as (a agtype);
+                '''
+            )
+        except asyncpg.exceptions.InternalServerError:
+            await transaction.rollback()
+        else:
+            print('unhandled cypher(cstring) function call error no longer occurring')
+
+        await conn.execute(
+            '''
+                SELECT * FROM cypher('testgraph', $$ CREATE (v:Person {name: $1}) $$) as (a agtype);
+            ''',
+            'Tom',
+        )
+
+asyncio.get_event_loop().run_until_complete(main())
