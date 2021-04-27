c517222b351e06e98407b0317c3f8fbf91e81452
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2021-04-27 10:45:06 +0200

    Clarify additional work for _from

--- app/models/es.py
+++ app/models/es.py
@@ -5,6 +5,7 @@ from typing import Any
 class ElasticSearchBody(BaseModel):
     # TODO: add custom validator that allows all aggs possibilities?
     aggs: dict = None
+    # from is a python keyword
     from_: int = None
     size: int = None
     # TODO: add custom validator that allows all sort possibilities?
@@ -18,10 +19,12 @@ class ElasticSearchBody(BaseModel):
 
     class Config:
         fields = {
+            # from is a python keyword
             'from_': 'from',
         }
 
     def dict(self, *args, **kwargs):
         d = super().dict(*args, **kwargs)
+        # from is a python keyword
         d['from'] = d.pop('from_')
         return d
