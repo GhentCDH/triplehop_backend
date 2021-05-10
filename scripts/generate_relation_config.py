9e716a9f32824dbffbdeed7e690a1c1cadccb079
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2021-05-10 14:50:22 +0200

    Fix script to extract relation config

--- scripts/generate_relation_config.py
+++ scripts/generate_relation_config.py
@@ -1,62 +1,68 @@
 import asyncio
-import databases
+import asyncpg
 import json
 import os
 
 import config
+import utils
 
 
 async def generate_config():
-    async with databases.Database(config.DATABASE_CONNECTION_STRING) as db:
-        for project_folder in os.listdir('human_readable_config'):
-            relations = {}
-            records = await db.fetch_all(
+    conn = await asyncpg.connect(**config.DATABASE)
+    for project_folder in os.listdir('human_readable_config'):
+        relations = {}
+        records = await utils.fetch(
+            conn,
+            '''
+                SELECT relation.id::text, relation.system_name
+                FROM app.relation
+                INNER JOIN app.project
+                    ON relation.project_id = project.id
+                WHERE project.system_name = :project_name;
+            ''',
+            {
+                'project_name': project_folder,
+            }
+        )
+        for record in records:
+            relations[record['system_name']] = {
+                'id': record['id'],
+            }
+
+        for relation in relations:
+            id = relations[relation]['id']
+            # todo: relations with multiple domains / ranges?
+            relations[relation]['domain'] = await utils.fetchval(
+                conn,
                 '''
-                    SELECT relation.id::text, relation.system_name
-                    FROM app.relation
-                    INNER JOIN app.project
-                        ON relation.project_id = project.id
-                    WHERE project.system_name = :project_name;
+                    SELECT entity.system_name
+                    FROM app.relation_domain
+                    INNER JOIN app.entity
+                        ON relation_domain.entity_id = entity.id
+                    WHERE relation_domain.relation_id = :relation_id;
                 ''',
                 {
-                    'project_name': project_folder,
+                    'relation_id': id,
                 }
             )
-            for record in records:
-                relations[record['system_name']] = {
-                    'id': record['id'],
+            relations[relation]['range'] = await utils.fetchval(
+                conn,
+                '''
+                    SELECT entity.system_name
+                    FROM app.relation_range
+                    INNER JOIN app.entity
+                        ON relation_range.entity_id = entity.id
+                    WHERE relation_range.relation_id = :relation_id;
+                ''',
+                {
+                    'relation_id': id,
                 }
+            )
 
-            for relation in relations:
-                id = relations[relation]['id']
-                # todo: relations with multiple domains / ranges?
-                relations[relation]['domain'] = await db.fetch_val(
-                    '''
-                        SELECT entity.system_name
-                        FROM app.relation_domain
-                        INNER JOIN app.entity
-                            ON relation_domain.entity_id = entity.id
-                        WHERE relation_domain.relation_id = :relation_id;
-                    ''',
-                    {
-                        'relation_id': id,
-                    }
-                )
-                relations[relation]['range'] = await db.fetch_val(
-                    '''
-                        SELECT entity.system_name
-                        FROM app.relation_range
-                        INNER JOIN app.entity
-                            ON relation_range.entity_id = entity.id
-                        WHERE relation_range.relation_id = :relation_id;
-                    ''',
-                    {
-                        'relation_id': id,
-                    }
-                )
+        with open(f'human_readable_config/{project_folder}/relations.json', 'w') as f:
+            json.dump(relations, f, indent=4)
 
-            with open(f'human_readable_config/{project_folder}/relations.json', 'w') as f:
-                json.dump(relations, f, indent=4)
+    await conn.close()
 
 
 def main():
