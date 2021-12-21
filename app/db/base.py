2f88ce710caba9bb8ed72351358e44e02224af39
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2021-12-21 13:36:58 +0100

    WIP (not working for multiple simultaneous requests)

--- app/db/base.py
+++ app/db/base.py
@@ -10,8 +10,7 @@ class BaseRepository:
     def __init__(self, pool: asyncpg.pool.Pool) -> None:
         self._pool = pool
         self._connection = None
-        self._connection_acquired = False
-        self._age_initialized = False
+        self._num_connection_required = 0
 
     @staticmethod
     def _render(query_template: str, params: typing.Dict[str, typing.Any] = None):
@@ -23,24 +22,48 @@ class BaseRepository:
         return [query, args]
 
     @asynccontextmanager
+    async def connection(self) -> None:
+        try:
+            print('connection context try')
+            if self._num_connection_required == 0:
+                self._connection = await self._pool.acquire()
+            self._num_connection_required += 1
+            print(self._num_connection_required)
+            yield self._connection
+        finally:
+            print('connection context finally')
+            self._num_connection_required -= 1
+            print(self._num_connection_required)
+            if self._num_connection_required == 0:
+                print('connection context connection released')
+                await self._pool.release(self._connection)
+
+    @asynccontextmanager
     async def transaction(self) -> None:
         try:
-            if not self._connection_acquired:
+            print('transaction context try')
+            if self._num_connection_required == 0:
                 self._connection = await self._pool.acquire()
-                self._connection_acquired = True
+            self._num_connection_required += 1
+            print(self._num_connection_required)
             transaction = self._connection.transaction()
+            print('transaction initialized')
             await transaction.start()
+            print('transaction started')
             yield transaction
-        except:
+        except Exception as e:
+            print(e)
             await transaction.rollback()
             raise
         else:
             await transaction.commit()
         finally:
-            if not self._connection.is_in_transaction():
+            print('transaction context finally')
+            self._num_connection_required -= 1
+            print(self._num_connection_required)
+            if self._num_connection_required == 0:
+                print('transaction context connection released')
                 await self._pool.release(self._connection)
-                self._connection_acquired = False
-                self._age_initialized = False
 
     # Make sure apache Age queries can be executed
     @staticmethod
@@ -56,69 +79,43 @@ class BaseRepository:
             '''
         )
 
-    async def execute(
-        self,
-        query_template: str,
-        params: typing.Dict[str, typing.Any] = None,
-        age: bool = False,
-    ):
-        async with self._pool.acquire() as conn:
-            query, args = self.__class__._render(query_template, params)
-            if age:
-                async with conn.transaction():
-                    await self.__class__._init_age(conn)
-                    return await conn.execute(query, *args)
-            return await conn.execute(query, *args)
+    async def execute(self, *args, **kwargs):
+        return await self._db_call('execute', *args, **kwargs)
 
-    async def fetch(
-        self,
-        query_template: str,
-        params: typing.Dict[str, typing.Any] = None,
-        age: bool = False,
-    ):
-        async with self._pool.acquire() as conn:
-            query, args = self.__class__._render(query_template, params)
-            if age:
-                async with conn.transaction():
-                    await self.__class__._init_age(conn)
-                    result = await conn.fetch(query, *args)
-                    return result
-                    # return await conn.fetch(query, *args)
-            result = await conn.fetch(query, *args)
-            return result
-            # return await conn.fetch(query, *args)
+    async def fetch(self, *args, **kwargs):
+        return await self._db_call('fetch', *args, **kwargs)
 
-    async def fetchrow(
+    async def fetchrow(self, *args, **kwargs):
+        return await self._db_call('fetchrow', *args, **kwargs)
+
+    async def fetchval(self, *args, **kwargs):
+        return await self._db_call('fetchval', *args, **kwargs)
+
+    async def _db_call(
         self,
+        method: str,
         query_template: str,
         params: typing.Dict[str, typing.Any] = None,
         age: bool = False,
     ):
+        print(method)
+        print(query_template)
+        print(params)
         query, args = self.__class__._render(query_template, params)
-        if self._connection is not None:
-            if age:
-                async with self._connection.transaction():
-                    await self.__class__._init_age(self._connection)
-                    return await self._connection.fetchrow(query, *args)
-            return await self._connection.fetchrow(query, *args)
-
-        async with self._pool.acquire() as conn:
-            if age:
-                async with conn.transaction():
-                    await self.__class__._init_age(conn)
-                    return await conn.fetchrow(query, *args)
-            return await conn.fetchrow(query, *args)
-
-    async def fetchval(
-        self,
-        query_template: str,
-        params: typing.Dict[str, typing.Any] = None,
-        age: bool = False,
-    ):
-        async with self._pool.acquire() as conn:
-            query, args = self.__class__._render(query_template, params)
-            if age:
-                async with conn.transaction():
-                    await self.__class__._init_age(conn)
-                    return await conn.fetchval(query, *args)
-            return await conn.fetchval(query, *args)
+        if age:
+            async with self.transaction():
+                # TODO: try to use ag_catalog explicitly where needed
+                await self._connection.execute(
+                    '''
+                        SET search_path = ag_catalog, "$user", public;
+                    '''
+                )
+                await self._connection.execute(
+                    '''
+                        LOAD '$libdir/plugins/age';
+                    '''
+                )
+                return await getattr(self._connection, method)(query, *args)
+        else:
+            async with self.connection():
+                return await getattr(self._connection, method)(query, *args)
